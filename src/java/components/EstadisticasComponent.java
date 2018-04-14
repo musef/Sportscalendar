bfa2efc09360b499dcb8cb612e8e0a0a62dc93eb
musef <musef2904@gmail.com>
2018-04-14 11:43:00 +0200

    Modificaciones en estadisticas: - A nivel funcional, en component

--- src/java/components/EstadisticasComponent.java
+++ src/java/components/EstadisticasComponent.java
@@ -118,7 +118,7 @@ public class EstadisticasComponent {
         } catch (Exception ex) {
             log.error("ERROR: Algo ha ido mal obteniendo lista de actividades por parametros para el user "+user.getId()+" - mensaje: "+ex);
         }
-                      if (sumData!=null) System.out.println("VER LISTA data:"+sumData.size());
+        if (sumData!=null) System.out.println("VER LISTA data:"+sumData.get("time"));
         else System.out.println("VER LISTA DATA NULL");
         return sumData;
         
